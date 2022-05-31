bacb32fcfb4b3b952ef0c651945fc72bb2fe5a5f
raul reymundi <kinskispiral@gmail.com>
2022-05-30 22:55:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,67 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const fetchURL = "http://localhost:3000/toys"
+
+// fetch call
+fetch(fetchURL)
+.then(res => res.json())
+.then((toyArray) => toyArray.forEach((toyObj) => renderToy(toyObj)))
+
+// render toy card
+function renderToy(toyObj){
+  // individual elements
+  const toyDiv = document.createElement('div')
+  toyDiv.className = 'card'
+
+  const toyName = document.createElement('h2')
+  toyName.innerText = toyObj.name
+
+  const toyImg = document.createElement('img')
+  toyImg.src = toyObj.image
+  toyImg.className = 'toy-avatar'
+
+  const toyLikes = document.createElement('p')
+  toyLikes.innerText = 'Number of likes: ' + toyObj.likes
+// i couldn't figure out the patch likes so i did this instead
+  const likeBtn = document.createElement('button')
+  likeBtn.innerText = '❤️'
+  likeBtn.addEventListener('click', () => {
+    ++toyObj.likes
+    toyLikes.innerText = `Number of likes: ${toyObj.likes}`
+  })
+  // create card
+  toyDiv.append(toyName, toyImg, toyLikes, likeBtn)
+  // append card to div
+  const toyCollection = document.getElementById('toy-collection')
+     toyCollection.appendChild(toyDiv)
+}
+
+//form event handler to prevent default
+const form = document.querySelector('.add-toy-form')
+
+form.addEventListener('submit', submitHandler)
+
+function submitHandler(event){
+  event.preventDefault()
+  // capture form data
+  const newToy = {
+    name: event.target.name.value,
+    likes: 0,
+    image: event.target.image.value
+  }
+
+  window.scrollTo(0,document.body.scrollHeight)
+  renderToy(newToy)
+  event.target.reset()
+  
+  // post new card
+  fetch(fetchURL, {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(newToy)
+  })
+
+}
+
